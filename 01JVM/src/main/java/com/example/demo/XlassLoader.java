107c66272c646f6f3ada5eab5f47ee81b3f3f3dc
zhaobaozhi <zhaobaozhi@yuewen.com>
2021-11-07 16:51:05 +0800

    [feat]作业2 自定义classloader加载.xlass文件

---
+++ 01JVM/src/main/java/com/example/demo/XlassLoader.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * 作业2 自定义classloader,加载hello.xlass文件
+ */
+public class XlassLoader extends ClassLoader{
+
+    public static void main(String[] args) throws Exception {
+        String className = "Hello";
+        String methodName = "hello";
+        // 创建类加载器
+        ClassLoader classLoader = new XlassLoader();
+        // 加载相应的类
+        Class<?> clazz = classLoader.loadClass(className);
+        // 看看里面有些什么方法
+        for (Method m : clazz.getDeclaredMethods()) {
+            System.out.println(clazz.getSimpleName() + "." + m.getName());
+        }
+        // 创建对象
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        // 调用实例方法
+        Method method = clazz.getMethod(methodName);
+        method.invoke(instance);
+    }
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Path path = Paths.get("/Users/zhaobaozhi/develop/learn_git/01JVM/src/main/resources/Hello.xlass");
+        byte[] helloBase64 = new byte[0];
+        try {
+            helloBase64 = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] res = decode(helloBase64);
+
+        return defineClass(name, res, 0, res.length);
+    }
+
+
+
+    // 解码
+    private static byte[] decode(byte[] byteArray) {
+        byte[] targetArray = new byte[byteArray.length];
+        for (int i = 0; i < byteArray.length; i++) {
+            targetArray[i] = (byte) (255 - byteArray[i]);
+        }
+        return targetArray;
+    }
+
+}
